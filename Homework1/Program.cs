ea1e769ce8a9bdd8d9abbac30ff3f3020cf25e1d
Igor <igor_parygin@mail.ru>
2022-09-18 22:39:47 +0300

    задача 2 из домашнего задания

---
+++ Homework1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Ввидите первое число ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Ввидите второе число ");
+int b = Convert.ToInt32 (Console.ReadLine());
+if (a > b) Console.WriteLine(a + " больше " + b + " меньше");
+else Console.WriteLine(a + " меньше " + b + " больше");
